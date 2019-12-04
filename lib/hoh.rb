0d08f035482197f0711b6b1a591101c29d68ff81
vincent servio <vincentservio@gmail.com>
2019-12-04 02:51:30 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,7 +31,22 @@
 # }
 
 def naming_system
-  # Remember:
+{
+   label: "Kingdom",
+   sub_category: {
+     label: "Phylum",
+     sub_category: {
+       label: "Class",
+       sub_category:{
+         label: "Order",
+         sub_category: {
+           label: "Family",
+           sub_category: {
+             label: "Genus",
+             sub_category: {
+               label: "Species",
+               sub_category: nil  }}}}}}}
+#kingdom = {:Phylum=>{}, :Class=>{}, :Order=>{}}
   #  Kingdom
   #  Phylum
   #  Class
