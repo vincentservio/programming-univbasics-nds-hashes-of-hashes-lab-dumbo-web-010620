69ab8fed5143504f0362e586f2fde66dedff7291
vincent servio <vincentservio@gmail.com>
2019-12-04 02:44:31 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -45,7 +45,7 @@ def naming_system
              label : "Genus"
              sub_category: {
                label: "Species"
-               sub_category: nil  }}}}}}}
+               sub_category: nil  } } } } } } }
    
  
  
