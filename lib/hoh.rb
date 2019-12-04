6ddd3a35a5bd00adb840ddb98d5268e8e5b55379
vincent servio <vincentservio@gmail.com>
2019-12-04 02:45:31 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -36,15 +36,15 @@ def naming_system
    sub_category: {
      label: "Phylum",
      sub_category: {
-       label: "Class"
+       label: "Class",
        sub_category: {
-         label: "Order"
+         label: "Order",
          sub_category: {
-           label: "Family"
+           label: "Family",
            sub_category: {
-             label : "Genus"
+             label : "Genus",
              sub_category: {
-               label: "Species"
+               label: "Species",
                sub_category: nil  } } } } } } }
    
  
